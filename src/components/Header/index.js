665dbd87018e3a8b35fded47f7f7719203e53338
mrefil <muefiloglu@protonmail.com>
2022-07-06 15:36:42 +0300

    header created

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,20 @@ import React from "react";
 
 function Header() {
     return (
-        <div className="flex flex-row justify-between items-center bg-yellow-500">
+        <div className="flex flex-row justify-between items-center bg-white border-b mx-4 p-4">
             <div>Logo</div>
-            <div>Menu</div>
+            <div className="flex flex-row">
+                <div className="px-2">
+                    <a href="/">Home</a>
+                </div>
+                <div className="px-2">
+                    <a href="/about">About</a>
+                </div>
+                <div className="px-2">Contact</div>
+                <div className="px-2">Blog</div>
+                <div className="px-2">Log In</div>
+                <div className="px-2">Sign Up</div>
+            </div>
         </div>
     )
 }
