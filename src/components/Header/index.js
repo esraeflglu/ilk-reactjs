7a94d5d511676b7f82f4e87c4612bcd5328a8396
mrefil <muefiloglu@protonmail.com>
2022-07-06 15:22:06 +0300

    Header, Anasayfa and Footer created

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="flex flex-row justify-between items-center bg-yellow-500">
+            <div>Logo</div>
+            <div>Menu</div>
+        </div>
+    )
+}
+
+export default Header;
